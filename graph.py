2f9cf7128c747fd7c795ca732dd39a234ad4beef
Mark Rivera <Mrivera1991@gmail.com>
2021-07-31 11:23:44 -0700

    finished graph shortest path algo

--- graph.py
+++ graph.py
@@ -21,49 +21,49 @@ class Graph:
     
     return self.edges
 
-  def findPath(self, start, end):
-    path = [start]
-    visited = []
-    queue = []
+  def findPath(self, start, end, path = []):
+    path = path + [start]
 
     # Is my starting point the same as my end point? If so, I'm done! :)
     if start == end:
-      path.append(start)
-      return 
+      return path
 
-    def breadthFirstSearch(node):
-      # Add my node to a visited list, to prevent going to nodes I've already been to
-      if node not in visited:
-        visited.append(node)
-      else:
-        return
+    shortest = None
 
-      # Check which nodes I am connected to and add them to a queue
-      for neighbor in self.graph[node]:
-        queue.append(neighbor)
+    # We're going to go through each node in the graph, starting from the start node til the end node:
+    for node in self.graph[start]:
 
-      # process those nodes
-      while queue:
-        breadthFirstSearch(queue.pop(0))
+      # If the node we are on is not in the path go visit it
+      if node not in path:
 
-    breadthFirstSearch(start)
+        # Using recursion over iteration
+        newPath = self.findPath(node, end, path)
 
-    return path
+        # If newPath exists (Not falsy)
+        if newPath:
+          # Check if shortest is None OR if length of newPath is less than current shortest
+          if not shortest or len(newPath) < len(shortest):
+
+            # if our new path length is lesser than the current shortest, replace it
+            shortest = newPath
+    
+    return shortest
+
   
 directedGraph = Graph()
 
 directedGraph.addEdge("a", "b")
 directedGraph.addEdge("a", "c")
-# directedGraph.addEdge("b", "c")
-# directedGraph.addEdge("c", "d")
-# directedGraph.addEdge("c", "e")
-# directedGraph.addEdge("c", "f")
-# directedGraph.addEdge("d", "g")
-# directedGraph.addEdge("d", "e")
+directedGraph.addEdge("b", "c")
+directedGraph.addEdge("c", "d")
+directedGraph.addEdge("c", "e")
+directedGraph.addEdge("c", "f")
+directedGraph.addEdge("d", "g")
+directedGraph.addEdge("d", "e")
 
 
 print(directedGraph.generate_edges())
 
-print(directedGraph.findPath("a", "c"))
+print(directedGraph.findPath("a", "g"))
 
 print(directedGraph.graph)
